51a176c660b7e068000fd4eb3337b03e5ae67370
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-01-17 17:33:18 +0300

    Initial Commit
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ plugin.go
@@ -0,0 +1,47 @@
+package memcached
+
+import (
+	"github.com/roadrunner-server/api/v2/plugins/config"
+	"github.com/roadrunner-server/api/v2/plugins/kv"
+	"github.com/roadrunner-server/errors"
+	"github.com/roadrunner-server/memcached/v2/memcachedkv"
+	"go.uber.org/zap"
+)
+
+const (
+	PluginName     string = "memcached"
+	RootPluginName string = "kv"
+)
+
+type Plugin struct {
+	// config plugin
+	cfgPlugin config.Configurer
+	// logger
+	log *zap.Logger
+}
+
+func (p *Plugin) Init(log *zap.Logger, cfg config.Configurer) error {
+	if !cfg.Has(RootPluginName) {
+		return errors.E(errors.Disabled)
+	}
+
+	p.cfgPlugin = cfg
+	p.log = new(zap.Logger)
+	*p.log = *log
+	return nil
+}
+
+// Name returns plugin user-friendly name
+func (p *Plugin) Name() string {
+	return PluginName
+}
+
+func (p *Plugin) KvFromConfig(key string) (kv.Storage, error) {
+	const op = errors.Op("boltdb_plugin_provide")
+	st, err := memcachedkv.NewMemcachedDriver(p.log, key, p.cfgPlugin)
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+
+	return st, nil
+}
