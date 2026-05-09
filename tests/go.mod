26a0867e036a56892d3db9f124e570a75baff14a
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-05-09 13:21:13 +0200

    fix(tests): migrate KV proto from kv/v1 to kv/v2
    KV plugin server moved to api-go/v6/kv/v2 (KvRequest/KvItem/KvResponse,
    Ttl Duration). Tests still sent kv/v1 wire format → goridge codec
    returned 'cannot parse invalid wire-format data'.

--- tests/go.mod
+++ tests/go.mod
@@ -5,7 +5,7 @@ go 1.26
 toolchain go1.26.0
 
 require (
-	github.com/roadrunner-server/api/v4 v4.24.0
+	github.com/roadrunner-server/api-go/v6 v6.0.0-beta.4
 	github.com/roadrunner-server/config/v6 v6.0.0-beta.3
 	github.com/roadrunner-server/endure/v2 v2.6.2
 	github.com/roadrunner-server/goridge/v4 v4.0.0-beta.1
@@ -14,6 +14,7 @@ require (
 	github.com/roadrunner-server/memcached/v6 v6.0.0
 	github.com/roadrunner-server/rpc/v6 v6.0.0-beta.3
 	github.com/stretchr/testify v1.11.1
+	google.golang.org/protobuf v1.36.11
 )
 
 replace github.com/roadrunner-server/memcached/v6 => ../
@@ -33,7 +34,6 @@ require (
 	github.com/mattn/go-isatty v0.0.22 // indirect
 	github.com/pelletier/go-toml/v2 v2.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/roadrunner-server/api-go/v6 v6.0.0-beta.4 // indirect
 	github.com/roadrunner-server/api-plugins/v6 v6.0.0-beta.2 // indirect
 	github.com/roadrunner-server/errors v1.5.0 // indirect
 	github.com/roadrunner-server/tcplisten v1.5.2 // indirect
@@ -54,6 +54,5 @@ require (
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/sys v0.44.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
-	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- tests/storage_plugin_test.go
+++ tests/storage_plugin_test.go
@@ -11,7 +11,7 @@ import (
 	"testing"
 	"time"
 
-	kvProto "github.com/roadrunner-server/api/v4/build/kv/v1"
+	kvProto "github.com/roadrunner-server/api-go/v6/kv/v2"
 	"github.com/roadrunner-server/config/v6"
 	"github.com/roadrunner-server/endure/v2"
 	goridgeRpc "github.com/roadrunner-server/goridge/v4/pkg/rpc"
@@ -20,6 +20,7 @@ import (
 	"github.com/roadrunner-server/memcached/v6"
 	rpcPlugin "github.com/roadrunner-server/rpc/v6"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
 func TestMemcached(t *testing.T) {
@@ -94,11 +95,11 @@ func testRPCMethodsMemcached(t *testing.T) {
 	client := rpc.NewClientWithCodec(goridgeRpc.NewClientCodec(conn))
 
 	// add 5 second ttl
-	tt := time.Now().Add(time.Second * 5).Format(time.RFC3339)
+	tt := durationpb.New(time.Second * 5)
 
-	keys := &kvProto.Request{
+	keys := &kvProto.KvRequest{
 		Storage: "memcached-rr",
-		Items: []*kvProto.Item{
+		Items: []*kvProto.KvItem{
 			{
 				Key: "a",
 			},
@@ -111,9 +112,9 @@ func testRPCMethodsMemcached(t *testing.T) {
 		},
 	}
 
-	data := &kvProto.Request{
+	data := &kvProto.KvRequest{
 		Storage: "memcached-rr",
-		Items: []*kvProto.Item{
+		Items: []*kvProto.KvItem{
 			{
 				Key:   "a",
 				Value: []byte("aa"),
@@ -125,7 +126,7 @@ func testRPCMethodsMemcached(t *testing.T) {
 			{
 				Key:     "c",
 				Value:   []byte("cc"),
-				Timeout: tt,
+				Ttl: tt,
 			},
 			{
 				Key:   "d",
@@ -138,12 +139,12 @@ func testRPCMethodsMemcached(t *testing.T) {
 		},
 	}
 
-	ret := &kvProto.Response{}
+	ret := &kvProto.KvResponse{}
 	// Register 3 keys with values
 	err = client.Call("kv.Set", data, ret)
 	assert.NoError(t, err)
 
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.Has", keys, ret)
 	assert.NoError(t, err)
 	assert.Len(t, ret.GetItems(), 3) // should be 3
@@ -151,45 +152,45 @@ func testRPCMethodsMemcached(t *testing.T) {
 	// key "c" should be deleted
 	time.Sleep(time.Second * 7)
 
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.Has", keys, ret)
 	assert.NoError(t, err)
 	assert.Len(t, ret.GetItems(), 2) // should be 2
 
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.MGet", keys, ret)
 	assert.NoError(t, err)
 	assert.Len(t, ret.GetItems(), 2) // c is expired
 
-	tt2 := time.Now().Add(time.Second * 10).Format(time.RFC3339)
+	tt2 := durationpb.New(time.Second * 10)
 
-	data2 := &kvProto.Request{
+	data2 := &kvProto.KvRequest{
 		Storage: "memcached-rr",
-		Items: []*kvProto.Item{
+		Items: []*kvProto.KvItem{
 			{
 				Key:     "a",
-				Timeout: tt2,
+				Ttl: tt2,
 			},
 			{
 				Key:     "b",
-				Timeout: tt2,
+				Ttl: tt2,
 			},
 			{
 				Key:     "d",
-				Timeout: tt2,
+				Ttl: tt2,
 			},
 		},
 	}
 
 	// MEXPIRE
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.MExpire", data2, ret)
 	assert.NoError(t, err)
 
 	// TTL call is not supported for the memcached driver
-	keys2 := &kvProto.Request{
+	keys2 := &kvProto.KvRequest{
 		Storage: "memcached-rr",
-		Items: []*kvProto.Item{
+		Items: []*kvProto.KvItem{
 			{
 				Key: "a",
 			},
@@ -202,41 +203,41 @@ func testRPCMethodsMemcached(t *testing.T) {
 		},
 	}
 
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.TTL", keys2, ret)
 	assert.Error(t, err)
 	assert.Len(t, ret.GetItems(), 0)
 
 	// HAS AFTER TTL
 	time.Sleep(time.Second * 15)
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.Has", keys2, ret)
 	assert.NoError(t, err)
 	assert.Len(t, ret.GetItems(), 0)
 
 	// DELETE
-	keysDel := &kvProto.Request{
+	keysDel := &kvProto.KvRequest{
 		Storage: "memcached-rr",
-		Items: []*kvProto.Item{
+		Items: []*kvProto.KvItem{
 			{
 				Key: "e",
 			},
 		},
 	}
 
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.Delete", keysDel, ret)
 	assert.NoError(t, err)
 
 	// HAS AFTER DELETE
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.Has", keysDel, ret)
 	assert.NoError(t, err)
 	assert.Len(t, ret.GetItems(), 0)
 
-	dataClear := &kvProto.Request{
+	dataClear := &kvProto.KvRequest{
 		Storage: "memcached-rr",
-		Items: []*kvProto.Item{
+		Items: []*kvProto.KvItem{
 			{
 				Key:   "a",
 				Value: []byte("aa"),
@@ -260,24 +261,24 @@ func testRPCMethodsMemcached(t *testing.T) {
 		},
 	}
 
-	clr := &kvProto.Request{Storage: "memcached-rr"}
+	clr := &kvProto.KvRequest{Storage: "memcached-rr"}
 
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	// Register 3 keys with values
 	err = client.Call("kv.Set", dataClear, ret)
 	assert.NoError(t, err)
 
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.Has", dataClear, ret)
 	assert.NoError(t, err)
 	assert.Len(t, ret.GetItems(), 5) // should be 5
 
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.Clear", clr, ret)
 	assert.NoError(t, err)
 
 	time.Sleep(time.Second * 2)
-	ret = &kvProto.Response{}
+	ret = &kvProto.KvResponse{}
 	err = client.Call("kv.Has", dataClear, ret)
 	assert.NoError(t, err)
 	assert.Len(t, ret.GetItems(), 0) // should be 5
